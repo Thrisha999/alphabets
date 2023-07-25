9c8f13615a68705ff94fef01454667efc0ae7b16
Thrisha999 <101519219+Thrisha999@users.noreply.github.com>
2023-07-25 22:37:27 +0530

    Add files via upload

---
+++ Alphabets.java
@@ -0,0 +1,13 @@
+//printing alphabets
+import java.util.Scanner;
+class Alphabets
+{
+  public static void main(String args[])
+  {
+    char i;
+    for(i=65;i<=90;i++)
+    {
+      System.out.print(" "+i);
+    }
+  }
+}
